184fb99bc8aa84086359cafb98576114868a4319
zydl <68770882+Zhongyang-debug@users.noreply.github.com>
2021-11-14 19:03:36 +0800

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -3,6 +3,7 @@ import time
 import torch
 from src.pit_criterion import cal_loss
 from torch.utils.tensorboard import SummaryWriter
+import gc
 
 
 class Trainer(object):
@@ -13,7 +14,7 @@ class Trainer(object):
         self.model = model
         self.optimizer = optimizer
 
-        # training config
+        # Training config
         self.use_cuda = config["train"]["use_cuda"]  # 是否使用 GPU
         self.epochs = config["train"]["epochs"]  # 训练批次
         self.half_lr = config["train"]["half_lr"]  # 是否调整学习率
@@ -46,20 +47,20 @@ class Trainer(object):
         self._reset()
 
     def _reset(self):
-        # 是否接着原来进度训练
         if self.continue_from:
             # 接着原来进度训练
             print('Loading checkpoint model %s' % self.continue_from)
+            package = torch.load(self.save_folder + self.continue_from)
 
-            if torch.cuda.is_available():
-                map_location = "cuda"
-            else:
-                map_location = "cpu"
-
-            package = torch.load(self.save_folder+self.continue_from, map_location=map_location)
-
-            if isinstance(self.model, torch.nn.DataParallel):
-                self.model = self.model.module
+            # """
+            #     https://blog.csdn.net/qq_41563738/article/details/102913920
+            # """
+            # from collections import OrderedDict
+            # new_state_dict = OrderedDict()
+            # for key, value in package['state_dict'].items():
+            #     name = 'module.' + key
+            #     new_state_dict[name] = value
+            # self.model.load_state_dict(new_state_dict)
 
             self.model.load_state_dict(package['state_dict'])
             self.optimizer.load_state_dict(package['optim_dict'])
@@ -73,9 +74,7 @@ class Trainer(object):
             self.start_epoch = 0
 
     def train(self):
-
         for epoch in range(self.start_epoch, self.epochs):
-
             print("Train Start...")
 
             self.model.train()  # 将模型设置为训练模式
@@ -84,7 +83,10 @@ class Trainer(object):
 
             tr_loss = self._run_one_epoch(epoch)  # 训练模型
 
-            self.write.add_scalar("train loss", tr_loss, epoch + 1)  # 写入损失
+            gc.collect()
+            torch.cuda.empty_cache()
+
+            self.write.add_scalar("train loss", tr_loss, epoch+1)
 
             end_time = time.time()  # 训练结束时间
             run_time = end_time - start_time  # 训练时间
@@ -95,14 +97,14 @@ class Trainer(object):
 
             if self.checkpoint:
                 # 保存每一个训练模型
-                file_path = os.path.join(self.save_folder, 'epoch%d.pth.tar' % (epoch+1))
-                                                    
+                file_path = os.path.join(self.save_folder, 'epoch%d.pth.tar' % (epoch + 1))
+
                 if isinstance(self.model, torch.nn.DataParallel):
                     self.model = self.model.module
 
                 torch.save(self.model.serialize(self.model,
                                                 self.optimizer,
-                                                epoch+1,
+                                                epoch + 1,
                                                 tr_loss=self.tr_loss,
                                                 cv_loss=self.cv_loss), file_path)
 
@@ -127,10 +129,8 @@ class Trainer(object):
 
             # 是否调整学习率
             if self.half_lr:
-
                 # 验证损失是否提升
                 if val_loss >= self.prev_val_loss:
-
                     self.val_no_improve += 1  # 统计没有提升的次数
 
                     # 如果训练 3 个 epoch 没有提升，学习率减半
@@ -145,15 +145,10 @@ class Trainer(object):
                     self.val_no_improve = 0
 
             if self.halving:
-
                 optime_state = self.optimizer.state_dict()
-
                 optime_state['param_groups'][0]['lr'] = optime_state['param_groups'][0]['lr']/2.0
-
                 self.optimizer.load_state_dict(optime_state)
-
                 print('Learning rate adjusted to: {lr:.6f}'.format(lr=optime_state['param_groups'][0]['lr']))
-
                 self.halving = False
 
             self.prev_val_loss = val_loss  # 当前损失
@@ -167,9 +162,6 @@ class Trainer(object):
                 self.best_val_loss = val_loss  # 最小的验证损失值
 
                 file_path = os.path.join(self.save_folder, self.model_path)
-                
-                if isinstance(self.model, torch.nn.DataParallel):
-                    self.model = self.model.module
 
                 torch.save(self.model.serialize(self.model,
                                                 self.optimizer,
@@ -187,7 +179,7 @@ class Trainer(object):
         data_loader = self.tr_loader if not cross_valid else self.cv_loader  # 数据集切换
 
         for i, (data) in enumerate(data_loader):
-            # torch.Size([1, 32000]), tensor([32000], dtype=torch.int32), torch.Size([1, 2, 32000])
+
             padded_mixture, mixture_lengths, padded_source = data
 
             # 是否使用 GPU 训练
@@ -198,7 +190,9 @@ class Trainer(object):
 
             estimate_source = self.model(padded_mixture)  # 将数据放入模型
 
-            loss, _, _, _ = cal_loss(padded_source, estimate_source, mixture_lengths)
+            loss, max_snr, estimate_source, reorder_estimate_source = cal_loss(padded_source,
+                                                                               estimate_source,
+                                                                               mixture_lengths)
 
             if not cross_valid:
                 self.optimizer.zero_grad()
@@ -213,11 +207,11 @@ class Trainer(object):
 
             if i % self.print_freq == 0:
                 print('Epoch {0} | Iter {1} | Average Loss {2:.3f} | Current Loss {3:.6f} | {4:.1f} s/batch'.format(
-                    epoch + 1,
-                    i + 1,
-                    total_loss / (i + 1),
+                    epoch+1,
+                    i+1,
+                    total_loss/(i+1),
                     loss.item(),
-                    run_time / (i + 1)),
+                    run_time/(i+1)),
                     flush=True)
 
-        return total_loss / (i + 1)
+        return total_loss/(i+1)
